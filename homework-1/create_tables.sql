c547653bf9ed3387c6be6e61b01dd93ce5293c8d
IliaPr <ilya.prianichnikov@gmail.com>
2023-04-09 23:57:54 +0700

    homework-1 - done: script for DB was create

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+create table employees
+(
+employee_id smallserial primary key,
+first_name varchar(100),
+last_name varchar(100),
+title varchar(100),
+birth_date date,
+notes text
+);
+
+create table customers
+(
+customer_id varchar(50) primary key,
+company_name varchar(200),
+contact_name varchar(200)
+);
+
+create table orders
+(
+order_id int primary key,
+customer_id varchar(50) references customers(customer_id),
+employee_id smallserial references employees(employee_id),
+order_date date,
+ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,28 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+conn = psycopg2.connect(host='localhost', database = 'north', user = 'postgres', password = '150774')
+try:
+    with conn:
+        with conn.cursor()as cur:
+            with open('../homework-1/north_data/customers_data.csv', 'r') as file: #получение данных из файла cuctomers.csv
+                customers_data = csv.DictReader(file)
+                for data in customers_data:
+                    cur.execute('insert into customers values (%s, %s, %s)',
+                                (data['customer_id'], data['company_name'], data['contact_name'])) #добавление данных в БД
+            with open('../homework-1/north_data/employees_data.csv', 'r') as file: #получение данных из файла employees_data.csv
+                employees_data = csv.DictReader(file)
+                for id, data in enumerate(employees_data, 1):
+                    cur.execute('insert into employees values (%s, %s, %s, %s, %s, %s)',
+                                (id, data['first_name'], data['last_name'],
+                                 data['title'], data['birth_date'], data['notes'])) #добавление данных в БД
+                with open('../homework-1/north_data/orders_data.csv', 'r') as file: #получение данных из файла orders_data.csv
+                    orders_data = csv.DictReader(file)
+                    for data in orders_data:
+                        cur.execute('insert into orders values (%s, %s, %s, %s, %s)',
+                                    (data['order_id'], data['customer_id'],
+                                     data['employee_id'], data['order_date'], data['ship_city'])) #добавление данных в БД
+finally:
+    conn.close() #закрытие соединения с БД
+
